bc41f1418a155315f6c6f0c046d38fe9e6ffd512
Ivan Kovalenko <ivko_02@mail.ru>
2020-12-14 22:14:36 +0300

    change text button

---
+++ app/src/main/java/com/example/androidgit/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidgit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this,"YES? It is GIT!", Toast.LENGTH_SHORT).show()
+    }
+}
