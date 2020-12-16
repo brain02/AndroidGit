22d0c16e397bb054bb5b4489e2f294172c03e695
Ivan Kovalenko <ivko_02@mail.ru>
2020-12-17 00:27:47 +0300

    change 17.12.2020

--- app/src/main/java/com/example/androidgit/MainActivity.kt
+++ app/src/main/java/com/example/androidgit/MainActivity.kt
@@ -1,14 +1,45 @@
 package com.example.androidgit
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this,"123", Toast.LENGTH_SHORT).show()
+        //init component
+        val btnBack = findViewById<Button>(R.id.btnBack)
+        val btnNext = findViewById<Button>(R.id.btnNext)
+        val myIntent = Intent(this, SecondActivity::class.java)
+
+        btnBack.setOnClickListener {
+          //  toastMe(this,"Click Back")
+
+            val firstFragment: Fragment = FirstFragment()
+            val fragmentManager = supportFragmentManager
+            val ft: FragmentTransaction = fragmentManager.beginTransaction()
+            ft.replace(android.R.id.content, firstFragment)
+            ft.commit()
+        }
+
+        btnNext.setOnClickListener {
+            myIntent.putExtra("text","SecondActivity") //
+            startActivity(myIntent)
+        }
 
     }
+
+    //Всплывающее окно Toast
+    fun toastMe(context: Context,text:String) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+    }
+
 }
---
+++ app/src/main/java/com/example/androidgit/SecondActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidgit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        setTitle(intent.getStringExtra("text"))
+    }
+}
