6fcd08f07241c0df84d91baa4a68992b3dd10a22
Ivan Kovalenko <ivko_02@mail.ru>
2020-12-17 16:35:40 +0300

    17.12.2020

--- app/src/main/java/com/example/androidgit/MainActivity.kt
+++ app/src/main/java/com/example/androidgit/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.example.androidgit
 
+import android.R.attr.button
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.Toast
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,25 +21,26 @@ class MainActivity : AppCompatActivity() {
         val btnNext = findViewById<Button>(R.id.btnNext)
         val myIntent = Intent(this, SecondActivity::class.java)
 
+        // переход во фрагмент
         btnBack.setOnClickListener {
-          //  toastMe(this,"Click Back")
-
-            val firstFragment: Fragment = FirstFragment()
-            val fragmentManager = supportFragmentManager
-            val ft: FragmentTransaction = fragmentManager.beginTransaction()
-            ft.replace(android.R.id.content, firstFragment)
-            ft.commit()
+            //toastMe(this,"Click Back")
+            val ft = supportFragmentManager.beginTransaction()
+                ft.add(android.R.id.content, FirstFragment())
+                ft.addToBackStack(null)
+                ft.commit()
         }
 
+        //переход в активити подключение DB
         btnNext.setOnClickListener {
-            myIntent.putExtra("text","SecondActivity") //
+            myIntent.putExtra("text", "SecondActivity") //
             startActivity(myIntent)
+            Services().hideKeyboard()
         }
 
     }
 
     //Всплывающее окно Toast
-    fun toastMe(context: Context,text:String) {
+    fun toastMe(context: Context, text: String) {
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
 
--- app/src/main/java/com/example/androidgit/SecondActivity.kt
+++ app/src/main/java/com/example/androidgit/SecondActivity.kt
@@ -1,7 +1,16 @@
 package com.example.androidgit
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import androidx.core.content.ContextCompat.getSystemService
 
 class SecondActivity : AppCompatActivity() {
 
@@ -9,5 +18,18 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         setTitle(intent.getStringExtra("text"))
+        // init var
+        val datalist = mutableListOf<String>("один","Два")
+        //init object
+        val listView = findViewById<ListView>(R.id.listView)
+        val btnConDB = findViewById<Button>(R.id.btnConDB)
+        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datalist)
+        listView.adapter=adapter
+
+        btnConDB.setOnClickListener {
+            Services().hideKeyboard()
+        }
+
     }
+
 }
